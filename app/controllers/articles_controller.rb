36e32cd827b9cf0216bd2eb56ed1add7f5482004
John Wilson <wilsonjohna@icloud.com>
2017-01-12 18:49:00 -0600

    added article update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
 		 @article = Article.new
 	end
 	
+	def edit
+  		@article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.new(article_params)
  
@@ -22,9 +26,20 @@ class ArticlesController < ApplicationController
   		end
 	end
 
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+
+
 	private
-  		def article_params
-    		params.require(:article).permit(:title, :text)
-  		end
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 
 end
